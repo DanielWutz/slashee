bf80a8288913b944ce48eeddc5e9291ea7f900db
Daniel Wutz <wutz@unternehmertum.de>
2018-09-11 17:03:19 +0200

    work in progress: Sheets API auth

--- Slashee/src/main/java/slashee/main/Slashee.java
+++ Slashee/src/main/java/slashee/main/Slashee.java
@@ -1,5 +1,7 @@
 package slashee.main;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +9,22 @@ public class Slashee {
 
 public static void main(String[] args) {
     	
-    	List<Map<String, String>> profilePerUser = SlackHandler.getProfilePerUser();
+    	/* List<Map<String, String>> profilePerUser = SlackHandler.getProfilePerUser();
     	for (Map<String, String> profile : profilePerUser) {
     		for (Map.Entry<String, String> field : profile.entrySet()) {
     		    Printer.info("Key = " + field.getKey() + ", Value = " + field.getValue());
     		}
     		Printer.info("=====================");
     	}
-    	
+    	*/
+		
+		try {
+			SheetsHandler.main(args);
+		} catch (IOException | GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			Printer.error(e.getStackTrace().toString());
+		}
+	
     	/*
     	Map<String, String> idToLabelMap = getIdToLabelMap();
     		
